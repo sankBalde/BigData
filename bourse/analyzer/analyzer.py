73f8ebe7fd67af53cbd97c7bccb6ba60560d9135
Abdoulaye Balde <abdoulaye.balde@epita.fr>
2024-03-25 16:08:30 +0100

    change is_file in timescaledb_model.py

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -11,14 +11,17 @@ def store_file(name, website):
     if db.is_file_done(name):
         return
     if website.lower() == "boursorama":
+        print("Yes bourse")
         try:
-            df = pd.read_pickle("bourse/data/boursorama/" + name)  # is this dir ok for you ?
+            df = pd.read_pickle("/home/bourse/data/" + name)  # is this dir ok for you ?
         except:
             year = name.split()[1].split("-")[0]
-            df = pd.read_pickle("bourse/data/boursorama/" + year + "/" + name)
+            df = pd.read_pickle("/home/bourse/data/" + year + "/" + name)
+            print(df)
         # to be finished
 
 
 if __name__ == '__main__':
-    # store_file("compA 2020-01-01 09:02:02.532411", "boursorama")
+
+    store_file("amsterdam 2020-01-01 09:02:02.532411.bz2", "boursorama")
     print("Done")
